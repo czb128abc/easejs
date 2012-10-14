8e2c2881c5d07ddddf0a523617910463d2f2fce8
xianlihua <xianlihua@gmail.com>
2012-10-14 21:55:35 +0800

    change Tab to Spaces format

--- test/module.js
+++ test/module.js
@@ -2,199 +2,199 @@
 module('module');
 
 test('定义独立模块', 3, function () {
-	var moduleId = uid();
+    var moduleId = uid();
 
-	define(moduleId, function (require, exports) {
-		// console.log(app);
-		ok(true, '独立模块 ' + this.id + ' 定义并编译完成');
-		equal(this.id, moduleId, '在模块回调中获取到的模块Id与定义时一致');
-		equal(this.exports, exports, '模块回调中的 this 指向当前模块对象');
-	});
+    define(moduleId, function (require, exports) {
+        // console.log(app);
+        ok(true, '独立模块 ' + this.id + ' 定义并编译完成');
+        equal(this.id, moduleId, '在模块回调中获取到的模块Id与定义时一致');
+        equal(this.exports, exports, '模块回调中的 this 指向当前模块对象');
+    });
 });
 
 test('定义两个独立模块', 2, function () {
-	var moduleId = uid();
+    var moduleId = uid();
 
-	define(moduleId, function () {
-		ok(true, '模块 ' + moduleId + ' 定义并编译完成');
-	});
+    define(moduleId, function () {
+        ok(true, '模块 ' + moduleId + ' 定义并编译完成');
+    });
 
-	moduleId = uid();
-	define(moduleId, function () {
-		ok(true, '模块 ' + moduleId + ' 定义并编译完成');
-	});
+    moduleId = uid();
+    define(moduleId, function () {
+        ok(true, '模块 ' + moduleId + ' 定义并编译完成');
+    });
 });
 
 test('定义依赖模块，先定义的先执行', 3, function () {
-	var a = 'order_a';
-	var b = 'order_b';
+    var a = 'order_a';
+    var b = 'order_b';
 
-	define(a, function (require, exports) {
-		exports.a = function () {
-			return 'a';
-		};
-		ok(true, '模块 ' + a + ' 定义并编译完成，其具有公开方法 a()');
-	});
+    define(a, function (require, exports) {
+        exports.a = function () {
+            return 'a';
+        };
+        ok(true, '模块 ' + a + ' 定义并编译完成，其具有公开方法 a()');
+    });
 
-	define(b, function (require, exports) {
-		var _a = require('order_a');
-		ok(true, '模块 ' + b + ' 定义并编译完成，其依赖于 ' + a);
-		equal(_a.a(), 'a', '调用依赖模块的方法 ' + a + '.a(); 返回结果符合预期');
-	});
+    define(b, function (require, exports) {
+        var _a = require('order_a');
+        ok(true, '模块 ' + b + ' 定义并编译完成，其依赖于 ' + a);
+        equal(_a.a(), 'a', '调用依赖模块的方法 ' + a + '.a(); 返回结果符合预期');
+    });
 });
 
 test('定义依赖模块，先定义的后执行', 3, function () {
-	var a = 'order_reverse_a';
-	var b = 'order_reverse_b';
+    var a = 'order_reverse_a';
+    var b = 'order_reverse_b';
 
-	define(a, function (require) {
-		var _b = require('order_reverse_b');
+    define(a, function (require) {
+        var _b = require('order_reverse_b');
 
-		ok(true, '模块 ' + a + ' 定义并编译完成，其依赖于 ' + b);
-		equal(_b.b(), 'b', '调用依赖模块的方法 ' + b + '.b(); 返回结果符合预期');
-	});
+        ok(true, '模块 ' + a + ' 定义并编译完成，其依赖于 ' + b);
+        equal(_b.b(), 'b', '调用依赖模块的方法 ' + b + '.b(); 返回结果符合预期');
+    });
 
-	define(b, function (require, exports) {
-		exports.b = function () {
-			return 'b';
-		};
+    define(b, function (require, exports) {
+        exports.b = function () {
+            return 'b';
+        };
 
-		ok(true, '模块 ' + b + ' 定义并编译完成，含有公开方法 b()');
-	});
+        ok(true, '模块 ' + b + ' 定义并编译完成，含有公开方法 b()');
+    });
 });
 
 test('特殊模块：无参数定义', 1, function () {
-	throws(function () {
-		define();
-	}, Error, 'bad arguments!');
+    throws(function () {
+        define();
+    }, Error, 'bad arguments!');
 });
 
 test('特殊模块：仅一个参数，为 function', 1, function () {
-	define(function () {
-		ok(true, '模块 ' + this.id + ' 初始化完毕');
-	});
+    define(function () {
+        ok(true, '模块 ' + this.id + ' 初始化完毕');
+    });
 });
 
 test('特殊模块：仅一个参数，为 object', 2, function () {
-	define({
-		value: 100,
-		initialize: function () {
-			ok(true, '执行模块对象的 initialize 方法');
-			equal(this.value, 100, '可以读取模块对象的成员值');
-		}
-	});
+    define({
+        value: 100,
+        initialize: function () {
+            ok(true, '执行模块对象的 initialize 方法');
+            equal(this.value, 100, '可以读取模块对象的成员值');
+        }
+    });
 });
 
 test('#13 依赖的模块没有完全就绪，则不执行', 0, function () {
-	define('add', function (require, exports, module) {
-		module.exports = function (a, b) {
-			return a + b;
-		};
-	});
+    define('add', function (require, exports, module) {
+        module.exports = function (a, b) {
+            return a + b;
+        };
+    });
 
-	define(function (require) {
-		var add = require('add');
-		var minus = require('minus');
+    define(function (require) {
+        var add = require('add');
+        var minus = require('minus');
 
-		equal(add(1, 2), 3);
-	});
+        equal(add(1, 2), 3);
+    });
 });
 
 test('复杂依赖', 7, function () {
-	define('_a', function (require, exports) {
-		var d = require('_d');
+    define('_a', function (require, exports) {
+        var d = require('_d');
 
-		ok(true, '模块 _a 初始化完毕，依赖模块 _d');
-	});
+        ok(true, '模块 _a 初始化完毕，依赖模块 _d');
+    });
 
-	define('_b', function (require, exports) {
-		var d = require('_d');
+    define('_b', function (require, exports) {
+        var d = require('_d');
 
-		ok(true, '模块 _b 初始化完毕，依赖模块 _d');
-	});
+        ok(true, '模块 _b 初始化完毕，依赖模块 _d');
+    });
 
-	define('_c', function (require, exports) {
-		var d = require('_d');
+    define('_c', function (require, exports) {
+        var d = require('_d');
 
-		ok(true, '模块 _c 初始化完毕，依赖模块 _d');
-	});
+        ok(true, '模块 _c 初始化完毕，依赖模块 _d');
+    });
 
-	define('_e', function () {
-		ok(true, '模块 _e 初始化完毕');
-	});
+    define('_e', function () {
+        ok(true, '模块 _e 初始化完毕');
+    });
 
-	define('_f', function () {
-		ok(true, '模块 _f 初始化完毕');
-	});
+    define('_f', function () {
+        ok(true, '模块 _f 初始化完毕');
+    });
 
-	define('_g', function () {
-		ok(true, '模块 _g 初始化完毕');
-	});
+    define('_g', function () {
+        ok(true, '模块 _g 初始化完毕');
+    });
 
-	define('_d', function (require, exports) {
-		var e = require('_e');
-		var f = require('_f');
-		var g = require('_g');
+    define('_d', function (require, exports) {
+        var e = require('_e');
+        var f = require('_f');
+        var g = require('_g');
 
-		ok(true, '模块 _d 初始化完毕，依赖模块 _e, _f, _g');
-	});
+        ok(true, '模块 _d 初始化完毕，依赖模块 _e, _f, _g');
+    });
 });
 
 test('依赖自身', 3, function () {
-	define('self_deps', function (require, exports, module) {
-		exports.method = function () {
-			return 'method';
-		};
+    define('self_deps', function (require, exports, module) {
+        exports.method = function () {
+            return 'method';
+        };
 
-		var self = require('self_deps');
-		var another = require('self_another');
+        var self = require('self_deps');
+        var another = require('self_another');
 
-		ok(true, '依赖自己可以运行');
-		equal(self.method(), 'method', '可以读取自身的公开成员');
-	});
+        ok(true, '依赖自己可以运行');
+        equal(self.method(), 'method', '可以读取自身的公开成员');
+    });
 
-	define('self_another', function (require, exports) {
-		ok(true, 'self_another 初始化完毕');
-	});
+    define('self_another', function (require, exports) {
+        ok(true, 'self_another 初始化完毕');
+    });
 });
 
 test('重复定义模块', function () {
-	var mid = uid();
+    var mid = uid();
 
-	define(mid, function () {
-		ok(true, mid + ' 初始化完毕');
-	});
+    define(mid, function () {
+        ok(true, mid + ' 初始化完毕');
+    });
 
-	throws(function () {
-		define(mid, function () {});
-	}, Error, 'Module ' + mid + ' already defined!');
+    throws(function () {
+        define(mid, function () {});
+    }, Error, 'Module ' + mid + ' already defined!');
 });
 
 test('相互依赖', 1, function () {
-	ok(true, '相互依赖的模块不会编译');
+    ok(true, '相互依赖的模块不会编译');
 
-	define('circleA', function (require, exports, module) {
-		exports.a = 'a';
-		var b = require('circleB');
-		ok(false, 'circleA 初始化完毕，依赖模块 circleB');
-	});
+    define('circleA', function (require, exports, module) {
+        exports.a = 'a';
+        var b = require('circleB');
+        ok(false, 'circleA 初始化完毕，依赖模块 circleB');
+    });
 
-	define('circleB', function (require, exports, module) {
-		exports.b = 'b';
-		var a = require('circleA');
-		ok(false, 'circleB 初始化完毕，依赖模块 circleA');
-	});
+    define('circleB', function (require, exports, module) {
+        exports.b = 'b';
+        var a = require('circleA');
+        ok(false, 'circleB 初始化完毕，依赖模块 circleA');
+    });
 });
 
 test('异步加载模块（或js文件）', 3, function () {
-	define(function (require) {
-		ok(true, '模块初始化完毕');
-		stop();
+    define(function (require) {
+        ok(true, '模块初始化完毕');
+        stop();
 
-		require.async('http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js', function () {
-			start();
-			ok(true, '异步加载的 jquery.js 完毕');
-			equal($('#qunit').size(), 1, '页面中 $("div#qunit").size() == 1');
-		});
-	});
+        require.async('http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js', function () {
+            start();
+            ok(true, '异步加载的 jquery.js 完毕');
+            equal($('#qunit').size(), 1, '页面中 $("div#qunit").size() == 1');
+        });
+    });
 });
--- test/util.js
+++ test/util.js
@@ -2,201 +2,201 @@
 module('util');
 
 test('extend', 2, function () {
-	define(function (require) {
-		var util = require('util');
+    define(function (require) {
+        var util = require('util');
 
-		var obj = {};
-		var result = util.extend(obj, {'a': 1}, {'b': 2});
-		equal(result.a, 1, '为目标对象扩展 a 属性，值为 1');
-		equal(result.b, 2, '为目标对象扩展 b 属性，值为 2');
-	});
+        var obj = {};
+        var result = util.extend(obj, {'a': 1}, {'b': 2});
+        equal(result.a, 1, '为目标对象扩展 a 属性，值为 1');
+        equal(result.b, 2, '为目标对象扩展 b 属性，值为 2');
+    });
 });
 
 test('uid', 2, function () {
-	define(function (require) {
-		var util = require('util');
+    define(function (require) {
+        var util = require('util');
 
-		var uid = util.uid();
-		var uid2 = util.uid();
+        var uid = util.uid();
+        var uid2 = util.uid();
 
-		ok(/^EASE_NATIVE_[\d]+$/.test(uid), '生成Id字符串，类似 EASE_NATIVE_{Number} ');
-		ok(uid != uid2, '每次生成的值不重复');
-	});
+        ok(/^EASE_NATIVE_[\d]+$/.test(uid), '生成Id字符串，类似 EASE_NATIVE_{Number} ');
+        ok(uid != uid2, '每次生成的值不重复');
+    });
 });
 
 test('unique', 2, function () {
-	define(function (require) {
-		var util = require('util');
+    define(function (require) {
+        var util = require('util');
 
-		var array = [1, 1, 2, 3];
-		var result = util.unique(array);
-		deepEqual(result, [1, 2, 3], '数组进行了去重');
+        var array = [1, 1, 2, 3];
+        var result = util.unique(array);
+        deepEqual(result, [1, 2, 3], '数组进行了去重');
 
-		array = [1, '1', 2, 3, true, false];
-		result = util.unique(array);
-		deepEqual(result, array, '严格限制只去重相同类型的值');
-	});
+        array = [1, '1', 2, 3, true, false];
+        result = util.unique(array);
+        deepEqual(result, array, '严格限制只去重相同类型的值');
+    });
 });
 
 test('indexOf', 4, function () {
-	define(function (require) {
-		var util = require('util');
+    define(function (require) {
+        var util = require('util');
 
-		var array = [1, '1', 2, 3, true, 0];
-		var result = util.indexOf(array, 1);
-		var result2 = util.indexOf(array, '1');
-		var result3 = util.indexOf(array, true);
-		var result4 = util.indexOf(array, false);
+        var array = [1, '1', 2, 3, true, 0];
+        var result = util.indexOf(array, 1);
+        var result2 = util.indexOf(array, '1');
+        var result3 = util.indexOf(array, true);
+        var result4 = util.indexOf(array, false);
 
-		ok(result === 0, '数字查找');
-		ok(result2 === 1, '字符串查找');
-		ok(result3 === 4, 'Boolean 值查找');
-		ok(result4 === -1, '查找不存在的值');
-	});
+        ok(result === 0, '数字查找');
+        ok(result2 === 1, '字符串查找');
+        ok(result3 === 4, 'Boolean 值查找');
+        ok(result4 === -1, '查找不存在的值');
+    });
 });
 
 test('keys', 1, function () {
-	define(function (require) {
-		var util = require('util');
+    define(function (require) {
+        var util = require('util');
 
-		var object = {
-			'1': 1,
-			'true': true,
-			'key': 'key'
-		};
+        var object = {
+            '1': 1,
+            'true': true,
+            'key': 'key'
+        };
 
-		var result = util.keys(object);
-		deepEqual(result, ['1', 'true', 'key'], '取对象的键，以数组形式返回');
-	});
+        var result = util.keys(object);
+        deepEqual(result, ['1', 'true', 'key'], '取对象的键，以数组形式返回');
+    });
 });
 
 test('map', 2, function () {
-	define(function (require) {
-		var util = require('util');
+    define(function (require) {
+        var util = require('util');
 
-		var array = [1, 2, 3];
-		var result = util.map(array, function (item) {
-			return ++item;
-		});
+        var array = [1, 2, 3];
+        var result = util.map(array, function (item) {
+            return ++item;
+        });
 
-		deepEqual(result, [2, 3, 4], '将数组中的值 +1');
+        deepEqual(result, [2, 3, 4], '将数组中的值 +1');
 
-		result = util.map(array, function (item) {
-			return item.toFixed(2);
-		});
-		deepEqual(result, ['1.00', '2.00', '3.00'], '将数组中的值转换为两位小数字符串');
-	});
+        result = util.map(array, function (item) {
+            return item.toFixed(2);
+        });
+        deepEqual(result, ['1.00', '2.00', '3.00'], '将数组中的值转换为两位小数字符串');
+    });
 });
 
 
 test('filter', 1, function () {
-	define(function (require) {
-		var util = require('util');
+    define(function (require) {
+        var util = require('util');
 
-		var array = [-1, 0, 2, 3];
-		var result = util.filter(array, function (item) {
-			return item > 0;
-		});
+        var array = [-1, 0, 2, 3];
+        var result = util.filter(array, function (item) {
+            return item > 0;
+        });
 
-		deepEqual(result, [2, 3], '只返回数组中大于 0 的元素');
-	});
+        deepEqual(result, [2, 3], '只返回数组中大于 0 的元素');
+    });
 });
 
 test('isObject', 4, function () {
-	define(function (require) {
-		var util = require('util');
+    define(function (require) {
+        var util = require('util');
 
-		equal(util.isObject({}), true, '{} 是 Object');
-		equal(util.isObject([]), false, '[] 不是 Object');
-		equal(util.isObject(document.getElementById('#qunit')), false, 'DOM 对象不是 Object');
-		equal(util.isObject(null), false, 'null 不是 Object');
-	});
+        equal(util.isObject({}), true, '{} 是 Object');
+        equal(util.isObject([]), false, '[] 不是 Object');
+        equal(util.isObject(document.getElementById('#qunit')), false, 'DOM 对象不是 Object');
+        equal(util.isObject(null), false, 'null 不是 Object');
+    });
 });
 
 test('isArray', 4, function () {
-	define(function (require) {
-		var isArray = require('util').isArray;
+    define(function (require) {
+        var isArray = require('util').isArray;
 
-		equal(isArray([]), true, '[] 是 Array');
-		equal(isArray([1, 'string']), true, '[1, "string"] 是 Array');
-		equal(isArray({'0': 0, '1': 1}), false, "{'0': 0, '1': 1} 不是 Array");
-		equal(isArray(new Array()), true, 'new Array() 是 Array');
-	});
+        equal(isArray([]), true, '[] 是 Array');
+        equal(isArray([1, 'string']), true, '[1, "string"] 是 Array');
+        equal(isArray({'0': 0, '1': 1}), false, "{'0': 0, '1': 1} 不是 Array");
+        equal(isArray(new Array()), true, 'new Array() 是 Array');
+    });
 });
 
 test('isRegExp', 2, function () {
-	define(function (require) {
-		var isRegExp = require('util').isRegExp;
+    define(function (require) {
+        var isRegExp = require('util').isRegExp;
 
-		equal(isRegExp(/./), true, '/./ 是正则表达式');
-		equal(isRegExp(new RegExp('.', 'g')), true, "new RegExp('.', 'g') 是正则表达式");
-	});
+        equal(isRegExp(/./), true, '/./ 是正则表达式');
+        equal(isRegExp(new RegExp('.', 'g')), true, "new RegExp('.', 'g') 是正则表达式");
+    });
 });
 
 test('isDate', 1, function () {
-	define(function (require) {
-		var isDate = require('util').isDate;
+    define(function (require) {
+        var isDate = require('util').isDate;
 
-		equal(isDate(new Date()), true, 'new Date() 是日期对象');
-	});
+        equal(isDate(new Date()), true, 'new Date() 是日期对象');
+    });
 });
 
 test('isNumber', 7, function () {
-	define(function (require) {
-		var isNumber = require('util').isNumber;
+    define(function (require) {
+        var isNumber = require('util').isNumber;
 
-		equal(isNumber(1), true, '1 是 Number');
-		equal(isNumber(1.2), true, '1.2 是 Number');
-		equal(isNumber(.43), true, '.43 是 Number');
-		equal(isNumber(NaN), true, 'NaN 是 Number');
-		equal(isNumber(0), true, '0 是 Number');
-		equal(isNumber(false), false, 'false 不是 Number');
-		equal(isNumber("2"), false, '"2" 不是 Number');
-	});
+        equal(isNumber(1), true, '1 是 Number');
+        equal(isNumber(1.2), true, '1.2 是 Number');
+        equal(isNumber(.43), true, '.43 是 Number');
+        equal(isNumber(NaN), true, 'NaN 是 Number');
+        equal(isNumber(0), true, '0 是 Number');
+        equal(isNumber(false), false, 'false 不是 Number');
+        equal(isNumber("2"), false, '"2" 不是 Number');
+    });
 });
 
 test('isString', 4, function () {
-	define(function (require) {
-		var isString = require('util').isString;
+    define(function (require) {
+        var isString = require('util').isString;
 
-		equal(isString("string"), true, '"string" 是 String');
-		equal(isString(22), false, '22 不是 String');
-		equal(isString(true), false, 'true 不是 String');
-		equal(isString(({}).toString()), true, '({}).toString() 是 String');
-	});
+        equal(isString("string"), true, '"string" 是 String');
+        equal(isString(22), false, '22 不是 String');
+        equal(isString(true), false, 'true 不是 String');
+        equal(isString(({}).toString()), true, '({}).toString() 是 String');
+    });
 });
 
 test('isBoolean', 5, function () {
-	define(function (require) {
-		var isBoolean = require('util').isBoolean;
+    define(function (require) {
+        var isBoolean = require('util').isBoolean;
 
-		equal(isBoolean(true), true, 'true 是 Boolean');
-		equal(isBoolean(false), true, 'false 是 Boolean');
-		equal(isBoolean(undefined), false, 'undefined 不是 Boolean');
-		equal(isBoolean(null), false, 'null 不是 Boolean');
-		equal(isBoolean(0), false, '0 不是 Boolean');
-	});
+        equal(isBoolean(true), true, 'true 是 Boolean');
+        equal(isBoolean(false), true, 'false 是 Boolean');
+        equal(isBoolean(undefined), false, 'undefined 不是 Boolean');
+        equal(isBoolean(null), false, 'null 不是 Boolean');
+        equal(isBoolean(0), false, '0 不是 Boolean');
+    });
 });
 
 test('isFunction', 5, function () {
-	define(function (require) {
-		var isFunction = require('util').isFunction;
+    define(function (require) {
+        var isFunction = require('util').isFunction;
 
-		equal(isFunction(function(){}), true, 'function(){} 是 Function');
-		equal(isFunction(new Function()), true, 'new Function() 是 Function');
-		equal(isFunction({}), false, '{} 不是 Function');
-		equal(isFunction(/./), false, '/./ 不是 Function');
-		equal(isFunction(Date), true, 'Date 是 Function');
-	});
+        equal(isFunction(function(){}), true, 'function(){} 是 Function');
+        equal(isFunction(new Function()), true, 'new Function() 是 Function');
+        equal(isFunction({}), false, '{} 不是 Function');
+        equal(isFunction(/./), false, '/./ 不是 Function');
+        equal(isFunction(Date), true, 'Date 是 Function');
+    });
 });
 
 test('isElement', 4, function () {
-	define(function (require) {
-		var isElement = require('util').isElement;
+    define(function (require) {
+        var isElement = require('util').isElement;
 
-		equal(isElement(document.getElementById('qunit')), true, 'document.getElementById 获取到的是 DOM 元素');
-		equal(isElement(document.getElementsByTagName('body')[0]), true, 'document.getElementsByTagName 的一个元素为 DOM 元素');
-		equal(isElement(document.getElementsByName('__test')[0]), true, 'document.getElementsByName 的一个元素为 DOM 元素');
-		equal(isElement(document.links[0]), true, 'document.links 的一个元素为 DOM 元素');
-	});
+        equal(isElement(document.getElementById('qunit')), true, 'document.getElementById 获取到的是 DOM 元素');
+        equal(isElement(document.getElementsByTagName('body')[0]), true, 'document.getElementsByTagName 的一个元素为 DOM 元素');
+        equal(isElement(document.getElementsByName('__test')[0]), true, 'document.getElementsByName 的一个元素为 DOM 元素');
+        equal(isElement(document.links[0]), true, 'document.links 的一个元素为 DOM 元素');
+    });
 });
